f08860e00801b1bd59b5feb14a4a1613befe426e
JUNG SUNG YOON <xbox1111@naver.com>
2022-03-09 00:56:36 +0900

    1769

---
+++ Silver V/1769.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+function solution(str) {
+    let answer = 0;
+
+    function recur(str) {
+        if(str < 10) return str;
+        str = String(str).split('').reduce((acc,cur) => acc+ (+cur),0);
+        answer++;
+        return recur(str); 
+    }
+
+    recur(str);
+
+    return [answer,recur(str) % 3 === 0 ? 'YES' : 'NO']
+}
+
+console.log(solution(input).join('\n'));
