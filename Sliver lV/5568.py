c098cd1a0c14b694d513dab0eef127546e66522e
JUNG SUNG YOON <xbox1111@naver.com>
2023-03-08 02:34:58 +0900

    commit

---
+++ Sliver lV/5568.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+import sys
+input = sys.stdin.readline
+
+n, k = int(input()), int(input())
+cards = [input().rstrip() for _ in range(n)]
+res = set()
+for per in permutations(cards, k):
+    res.add(''.join(per))
+    
+print(len(res))
